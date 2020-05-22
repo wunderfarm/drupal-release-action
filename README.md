e0e1c15e93ca91d90e8d2ae65130c99b155c4922
Benedikt Kofler <benedikt.kofler@gmail.com>
2020-05-22 10:22:44 +0200

    setup WF Drupal build action

---
+++ README.md
@@ -0,0 +1,76 @@
+# WF Drupal Release Action
+
+This action builds WF Drupal projects, uploads the zipped artifacts to an AWS S3 Bucket and triggers the app deployment on the desired AWS OpsWorks Stack.
+
+## Inputs
+
+### `wf-webname`
+**Required** The unique name of the web. Default `"example2020"`.  
+
+### `aws-s3-bucket`
+**Required** The AWS S3 Bucket Name.  
+
+### `aws-access-key-id`
+**Required** The AWS S3 Access Key ID.  
+
+### `aws-secret-access-key`
+**Required** The AWS S3 Secred Access Key.  
+
+### `aws-region`
+The AWS S3 Region. Default `"eu-west-1"`  
+
+### `aws-opsworks-stack-id`
+The AWS Opsworks Stack ID.  
+
+### `aws-opsworks-app-id`
+The AWS Opsworks App ID.  
+
+## Example usage
+```yaml
+uses: wunderfarm/drupal-release-action@v1
+with:
+  wf-webname: 'example2020'
+  aws-s3-bucket: ${{ secrets.AWS_S3_BUCKET }}
+  aws-access-key-id: ${{ secrets.AWS_ACCESS_KEY_ID }}
+  aws-secret-access-key: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+  aws-region: 'eu-west-1'
+  aws-opsworks-stack-id: ${{ secrets.AWS_STACK_ID }}
+  aws-opsworks-app-id: ${{ secrets.AWS_APP_ID }}
+```
+
+## Package for distribution
+
+GitHub Actions will run the entry point from the action.yml. Packaging assembles the code into one file that can be checked in to Git, enabling fast and reliable execution and preventing the need to check in node_modules.
+
+Actions are run from GitHub repos.  Packaging the action will create a packaged action in the dist folder.
+
+Run package
+
+```bash
+npm run package
+```
+
+Since the packaged index.js is run from the dist folder.
+
+```bash
+git add dist
+```
+
+## Create a release branch
+
+Users shouldn't consume the action from master since that would be latest code and actions can break compatibility between major versions.
+
+Checkin to the v1 release branch
+
+```bash
+$ git checkout -b v1
+$ git commit -a -m "v1 release"
+```
+
+```bash
+$ git push origin v1
+```
+
+Your action is now published!
+
+See the [versioning documentation](https://github.com/actions/toolkit/blob/master/docs/action-versioning.md)
---
+++ index.js
@@ -0,0 +1,83 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const { execSync } = require('child_process')
+const fs = require('fs')
+const AWS = require('aws-sdk')
+
+try {
+    const wfWebname = core.getInput('wf-webname')
+    const awsS3Bucket = core.getInput('aws-s3-bucket')
+    const awsAccessKeyId = core.getInput('aws-access-key-id')
+    const awsSecretAccessKey = core.getInput('aws-secret-access-key')
+    const awsRegion = core.getInput('aws-region')
+    const awsOpsworksStackId = core.getInput('aws-opsworks-stack-id')
+    const awsOpsworksAppId = core.getInput('aws-opsworks-app-id')
+    AWS.config = new AWS.Config()
+    AWS.config.accessKeyId = awsAccessKeyId
+    AWS.config.secretAccessKey = awsSecretAccessKey
+    AWS.config.region = awsRegion ? awsRegion : 'eu-west-1'
+    const s3 = new AWS.S3()
+    const opsworks = new AWS.OpsWorks({apiVersion: '2013-02-18'})
+
+    let githubRef = github.context.ref
+    let commitSha = github.context.sha
+    let eventPayload = github.context.payload
+    let message = ''
+    if (typeof(eventPayload.release) !== 'undefined') {
+        message = 'Release: ' + eventPayload.release.name + ' - "' + eventPayload.release.body + '" '
+    } else if (typeof(eventPayload.commits) !== 'undefined') {
+        message = 'Commit: "' + eventPayload.commits[0].message + '" '
+    }
+    message = message + 'Ref: ' + githubRef + ' (' + commitSha.substring(0,7) + ')'
+    console.log(execSync('composer validate').toString())
+    console.logexecSync('composer install --prefer-dist --no-progress --no-suggest').toString())
+    execSync('mkdir dist').toString()
+    execSync('cp -R vendor dist').toString()
+    execSync('cp -R *.php dist').toString()
+    execSync('cp -R robots.txt dist').toString()
+    execSync('cp -R ads.txt dist').toString()
+    execSync('cp -R includes dist').toString()
+    execSync('cp -R misc dist').toString()
+    execSync('cp -R modules dist').toString()
+    execSync('cp -R profiles dist').toString()
+    execSync('cp -R scripts dist').toString()
+    execSync('cp -R sites dist').toString()
+    execSync('cp -R themes dist').toString()
+    execSync('cp -R web.config dist').toString()
+    execSync(`zip -rq ${wfWebname} ./dist`).toString()
+    
+    let filename = wfWebname + '.zip'
+
+    let file = fs.readFileSync(filename)
+    let s3params = {
+        Bucket: awsS3Bucket,
+        Key: wfWebname + '/' + filename,
+        Body: file
+    }
+    let opsworksParams = {
+        Command: {
+            Name: 'deploy'
+        },
+        StackId: awsOpsworksStackId,
+        AppId: awsOpsworksAppId,
+        Comment: message
+    }
+
+    s3.upload(s3params, function (err, data) {
+        if (err) {
+            core.setFailed(err.toString())
+            throw err
+        }
+        console.log(`File uploaded successfully. ${data.Location}`)
+
+        opsworks.createDeployment(opsworksParams, function (err, data) {
+            if (err) {
+                core.setFailed(err.toString())
+                throw err
+            }
+            console.log(`App successfully deployed. ${data.DeploymentId}`)
+        })
+    })
+} catch (error) {
+    core.setFailed(error.message)
+}
